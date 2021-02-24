33bffdd033c80c689987cafaf5d8a1f5e3b5c2a7
geraldarzy <geraldarzola@gmail.com>
2021-02-24 15:42:35 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
   private
+
+  def print_out_params
+
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
